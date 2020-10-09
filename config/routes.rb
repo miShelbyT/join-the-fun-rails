abaf198ff7f5758233dcd4b3ecf1d153fd975cd1
miShelbyT <mi.shelbyrose@gmail.com>
2020-10-08 20:35:34 -0400

    lab done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+	resources :taxis
+	resources :rides
+	resources :passengers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
